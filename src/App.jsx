1ad968f7dd29b03d874c28bb744849167fce3e9c
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2024-12-30 00:34:58 +0500

    Add pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import ProductsPage from "./pages/ProductsPage"
+import OverviewPage from "./pages/OverviewPage"
+
+
+
+function App() {
+
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hiddenl">
+      
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
