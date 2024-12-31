b8466c812934ca29c9b197b138ab3bf4c194c976
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-01 00:39:33 +0500

    Add sidebar items with icons and paths

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,50 @@
+import { BarChart2, ShoppingBag, User, Users } from 'lucide-react'
 import React from 'react'
+
+const SIDEBAR_ITEMS = [
+  {
+    name: "Overview",
+    icon: BarChart2,  
+    color : "#6366f1",
+    path: "/",
+  },
+  {
+    name: "Products",
+    icon: ShoppingBag,  
+    color : "#8B5CF6",
+    path: "/products",
+  },
+  {
+    name: "Users",
+    icon: Users,  
+    color : "#EC4899",
+    path: "/users",
+  },
+  {
+    name: "Overview",
+    icon: BarChart2,  
+    color : "#6366f1",
+    path: "/",
+  },
+  {
+    name: "Overview",
+    icon: BarChart2,  
+    color : "#6366f1",
+    path: "/",
+  },
+  {
+    name: "Overview",
+    icon: BarChart2,  
+    color : "#6366f1",
+    path: "/",
+  },
+  {
+    name: "Overview",
+    icon: BarChart2,  
+    color : "#6366f1",
+    path: "/",
+  },
+]
 
 function Sidebar() {
   return (
