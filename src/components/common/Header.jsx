3c9797641a103bfed06d7466ff26c3b51d999da1
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-02 23:57:07 +0500

    Add Header component with basic structure and styling

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header({ title }) {
+  return (
+    <header className='border-gray-700 bg-opacity-50 backdrop-blur-md shadow-lg bg-gray-800 border-b p-4'>
+      <div>
+        <h1></h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
