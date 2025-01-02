fa01501fdd58a73bf070a1dd29e366ff483689ea
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-03 00:01:35 +0500

    Enhance Header component styling and add title prop rendering

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Header({ title }) {
   return (
-    <header className='border-gray-700 bg-opacity-50 backdrop-blur-md shadow-lg bg-gray-800 border-b p-4'>
-      <div>
-        <h1></h1>
+    <header className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700'>
+      <div className='max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8'>
+        <h1 className='text-2xl font-semibold text-gray-100'>{title}</h1>
       </div>
     </header>
   )
