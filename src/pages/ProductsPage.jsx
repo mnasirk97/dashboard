c53638a80b931d781469dcd902bd7bce29459d06
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-09 02:01:24 +0500

    Add Header component to ProductsPage for improved layout

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../components/common/Header'
 
 function ProductsPage() {
   return (
-    <div>
-      ProductsPage
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Overview" />
     </div>
   )
 }
