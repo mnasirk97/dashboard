12c3afe8703cf033e7aa1e61197206c5f38e31cd
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-09 02:02:56 +0500

    Update Header title in ProductsPage to reflect correct context

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -4,7 +4,7 @@ import Header from '../components/common/Header'
 function ProductsPage() {
   return (
     <div className="flex-1 overflow-auto relative z-10">
-      <Header title="Overview" />
+      <Header title="Products" />
     </div>
   )
 }
