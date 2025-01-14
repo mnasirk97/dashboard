96932d1910169f0b318a543fbbcfa1abf883a3d0
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-14 22:41:32 +0500

    Add route for OrdersPage to manage and visualize order statistics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import OverviewPage from "./pages/OverviewPage"
 import Sidebar from "./components/common/Sidebar"
 import UsersPage from "./pages/UsersPage"
 import SalesPage from "./pages/SalesPage"
+import OrdersPage from "./pages/OrdersPage"
 
 
 
@@ -25,6 +26,7 @@ function App() {
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage  />} />
       </Routes>
     </div>
   )
