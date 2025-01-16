399fe82978b4d5a92c1f10c4763e910ad8473e65
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-16 23:29:36 +0500

    Uncomment performance components in AnalyticsPage for enhanced analytics display

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -2,11 +2,10 @@ import Header from "../components/common/Header";
 
 import OverviewCards from "../components/analytics/OverviewCards";
 import RevenueChart from "../components/analytics/RevenueChart";
-// import RevenueChart from "../components/analytics/RevenueChart";
-// import ChannelPerformance from "../components/analytics/ChannelPerformance";
-// import ProductPerformance from "../components/analytics/ProductPerformance";
-// import UserRetention from "../components/analytics/UserRetention";
-// import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
+import ChannelPerformance from "../components/analytics/ChannelPerformance";
+import ProductPerformance from "../components/analytics/ProductPerformance";
+import UserRetention from "../components/analytics/UserRetention";
+import CustomerSegmentation from "../components/analytics/CustomerSegmentation";
 // import AIPoweredInsights from "../components/analytics/AIPoweredInsights";
 
 const AnalyticsPage = () => {
@@ -18,12 +17,12 @@ const AnalyticsPage = () => {
 				<OverviewCards />
 				<RevenueChart />
 
-				{/* <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
+				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
 					<ChannelPerformance />
 					<ProductPerformance />
 					<UserRetention />
 					<CustomerSegmentation />
-				</div> */}
+				</div>
 
 				{/* <AIPoweredInsights /> */}
 			</main>
