866b3bf5db372b44bc04112fe4e5f3ec7edf7415
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-01-16 23:12:48 +0500

    Add RevenueChart component to AnalyticsPage for enhanced revenue visualization

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/common/Header";
 
 import OverviewCards from "../components/analytics/OverviewCards";
+import RevenueChart from "../components/analytics/RevenueChart";
 // import RevenueChart from "../components/analytics/RevenueChart";
 // import ChannelPerformance from "../components/analytics/ChannelPerformance";
 // import ProductPerformance from "../components/analytics/ProductPerformance";
@@ -15,7 +16,7 @@ const AnalyticsPage = () => {
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
 				<OverviewCards />
-				{/* <RevenueChart /> */}
+				<RevenueChart />
 
 				{/* <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
 					<ChannelPerformance />
